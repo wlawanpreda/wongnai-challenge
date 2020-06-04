5dac93465168952f2f6c94adb07bfd2532b9f52f
Wiriya Lawanpreda <wiriya@MacBook-Air.local>
2020-06-04 23:31:12 +0700

    fix wrong port ui:5555 api:5556 and fix version compose

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5555);
+  await app.listen(5556);
 }
 bootstrap();
